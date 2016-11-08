968e1ea0499fd5b4dda9dba2891d6abe92f25605
odoyle <my_email>
2016-11-08 15:59:40 +0000

    ex vals

--- R_scripts/extreme_values_results.R
+++ R_scripts/extreme_values_results.R
@@ -10,7 +10,7 @@ missing_flag <- 'missing'
 x[x == "-99"] <- missing_flag
 
 ex_val_thrsh <- read.csv(paste0(path_meta, 'ex_val_thrsh.csv'),stringsAsFactors=FALSE, na.strings=c("","NA"))
-xx <- x #keep back up of x
+#xx <- x #keep back up of x
 
 #numerical
 nv_names <- vtype[vtype$Type == 'n',]
@@ -19,8 +19,8 @@ nx <- x[, which(names(x) %in% nv_names$ColumnName)]
 
 ex_val_thrsh_out <- data.frame(matrix(nrow = ncol(nx), ncol = 1))
 colnames(ex_val_thrsh_out) <- colnames(ex_val_thrsh)[1]
-ex_val_thrsh_out$Variable.Name <-  colnames(nx)
-ex_val_thrsh_out <- merge(x= ex_val_thrsh_out, y = ex_val_thrsh, by = "Variable.Name", all = TRUE)
+ex_val_thrsh_out$Variable <-  colnames(nx)
+ex_val_thrsh_out <- merge(x= ex_val_thrsh_out, y = ex_val_thrsh, by = "Variable", all = TRUE)
 
 ex_nx <-nx
 
@@ -42,3 +42,5 @@ for (i in 1:ncol(nx)) {
   x[[vname]] <- vdata
 }
 write.csv(ex_val_thrsh_out, file = paste0(path_output,"ex_val_thrsh_out.csv"), row.names = FALSE)
+
+write.csv(x, file = paste0(path_output,"ex_mtcars.csv"), row.names = FALSE)
